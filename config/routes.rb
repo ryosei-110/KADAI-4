8634e1e374a09cb7dc79a1183d6d0d7e34376b1d
ryosei-110 <ito.ryosei.infratop@gmail.com>
2023-04-07 13:23:47 +0000

    viewファイルの作成まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'homes/top'
+  devise_for :users
+  resources :books, only[:new, :index, :show]
+  resources :users, only[:show, :edit]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
